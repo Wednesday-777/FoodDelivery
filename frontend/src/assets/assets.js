e8cae5d810645d7e7bd18f2e01e5d227f244ec0b
Wednesday-777 <roysupriyaja@gmail.com>
2025-08-07 21:32:29 +0530

    Add files via upload

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,403 @@
+import logo from './logo.png'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import profile_image from './profile_image.png'
+import profile_icon from './profile_icon.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+import search_icon from './search_icon.png'
+import basket_icon from './basket_icon.png'
+import add_icon_green from './add_icon_green.png'
+import add_icon_white from './add_icon_white.png'
+import app_store from './app_store.png'
+import bag_icon from './bag_icon.png'
+import cross_icon from './cross_icon.png'
+import facebook_icon from './facebook_icon.png'
+import header_img from './header_img.png'
+import linkedin_icon from './linkedin_icon.png'
+import logout_icon from './logout_icon.png'
+import play_store from './play_store.png'
+import rating_starts from './rating_starts.png'
+import remove_icon_red from './remove_icon_red.png'
+import selector_icon from './selector_icon.png'
+import twitter_icon from './twitter_icon.png'
+
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+
+
+
+
+
+
+
+export const assets ={
+    logo,
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon,
+    search_icon,
+    basket_icon,
+    add_icon_green,
+    add_icon_white,
+    app_store,
+    bag_icon,
+    cross_icon,
+    facebook_icon,
+    header_img,
+    linkedin_icon,
+    logout_icon,
+    play_store,
+    profile_icon,
+    rating_starts,
+    remove_icon_red,
+    selector_icon,
+    twitter_icon,
+    menu_1,
+    menu_2,
+    menu_3,
+    menu_4,
+    menu_5,
+    menu_6,
+    menu_7,
+    menu_8,
+    food_1,
+    food_2,
+    food_3,
+    food_4,
+    food_5,
+    food_6,
+    food_7,
+    food_8,
+    food_9,
+    food_10,
+    food_11,
+    food_12,
+    food_13,
+    food_14,
+    food_15,
+    food_16,
+    food_17,
+    food_18,
+    food_19,
+    food_20,
+    food_21,
+    food_22,
+    food_23,
+    food_24,
+    food_25,
+    food_26,
+    food_27,
+    food_28,
+    food_29,
+    food_30,
+    food_31,
+    food_32,
+    
+
+}
+export const menu_list = [
+    {
+        menu_name : "Salad",
+        menu_image : menu_1
+    },
+    {
+        menu_name : "Rolls",
+        menu_image : menu_2
+    },
+    {
+        menu_name : "Deserts",
+        menu_image : menu_3
+    },
+    {
+        menu_name : "Sandwich",
+        menu_image : menu_4
+    },
+    {
+        menu_name : "Cakes",
+        menu_image : menu_5
+    },
+    {
+        menu_name : "Pure-veg",
+        menu_image : menu_6
+    },
+    {
+        menu_name : "Pasta",
+        menu_image : menu_7
+    },
+    {
+        menu_name : "Noodles",
+        menu_image : menu_8
+    }
+]
+export const food_list = [
+      {
+        _id : "1",
+        name : "Greek Salad",
+        image : food_1,
+        price : 12,
+        category : "Salad"
+      },
+      {
+        _id : "2",
+        name : "Veg Salad",
+        image : food_2,
+        price : 18,
+        category : "Salad"
+      },
+      {
+        _id : "3",
+        name : "Salad with wallnut",
+        image : food_3,
+        price : 21,
+        category : "Salad"
+      },
+      {
+        _id : "4",
+        name : "Paneer Salad",
+        image : food_4,
+        price : 25,
+        category : "Salad"
+      },
+      {
+        _id : "5",
+        name : "Veg Lasagna",
+        image : food_5,
+        price : 30,
+        category : "Rolls"
+      },
+      {
+        _id : "6",
+        name : "Butter Chicken Wrap",
+        image : food_6,
+        price : 33,
+        category : "Rolls"
+      },
+      {
+        _id : "7",
+        name : "Chicken Roll",
+        image : food_7,
+        price : 35,
+        category : "Rolls"
+      },
+      {
+        _id : "8",
+        name : "Crispy Fried Lasagna Rolls",
+        image : food_8,
+        price : 38,
+        category : "Rolls"
+      },
+      {
+        _id : "9",
+        name : "Vegan ice-cream",
+        image : food_9,
+        price : 30,
+        category : "Dessert"
+      },
+      {
+        _id : "10",
+        name : "Cottage cheese pancakes",
+        image : food_10,
+        price : 32,
+        category : "Dessert"
+      },
+      {
+        _id : "11",
+        name : "Strawberry panna cotta",
+        image : food_11,
+        price : 35,
+        category : "Dessert"
+      },
+      {
+        _id : "12",
+        name : "Vanilla ice-cream",
+        image : food_12,
+        price : 18,
+        category : "Dessert"
+      },
+      {
+        _id : "13",
+        name : "Club sandwich",
+        image : food_13,
+        price : 22,
+        category : "Sandwich"
+      },
+      {
+        _id : "14",
+        name : "BLT sandwich",
+        image : food_14,
+        price : 26,
+        category : "Sandwich"
+      },
+      {
+        _id : "15",
+        name : "Chicken salad sandwich",
+        image : food_15,
+        price : 28,
+        category : "Sandwich"
+      },
+      {
+        _id : "16",
+        name : "Banh-Mi",
+        image : food_16,
+        price : 28,
+        category : "Sandwich"
+      },
+      {
+        _id : "17",
+        name : "Cupcake",
+        image : food_17,
+        price : 28,
+        category : "Dessert"
+      },
+      
+      {
+        _id : "18",
+        name : "strawberry cheesecake",
+        image : food_18,
+        price : 24,
+        category : "Dessert"
+      },
+      {
+        _id : "19",
+        name : "Blueberry cake",
+        image : food_19,
+        price : 38,
+        category : "Dessert"
+      },
+      {
+        _id : "20",
+        name : "Mango cheesecake",
+        image : food_20,
+        price : 25,
+        category : "Dessert"
+      },
+      {
+        _id : "21",
+        name : "Eggplant salad",
+        image : food_21,
+        price : 22,
+        category : "Salad"
+      },
+      {
+        _id : "22",
+        name : "Roasted Cauliflower",
+        image : food_17,
+        price : 28,
+        category : "Salad"
+      },
+      {
+        _id : "23",
+        name : "Caeser salad",
+        image : food_23,
+        price : 29,
+        category : "Salad"
+      },
+      {
+        _id : "24",
+        name : "Green fried rice with poached egg",
+        image : food_24,
+        price : 30,
+        category : "Pure-veg"
+      },
+      {
+        _id : "25",
+        name : "Cheese macaroni",
+        image : food_25,
+        price : 27,
+        category : "Pasta"
+      },
+      {
+        _id : "26",
+        name : "Red sauce pasta",
+        image : food_26,
+        price : 29,
+        category : "Pasta"
+      },
+      {
+        _id : "27",
+        name : "White sauce pasta",
+        image : food_27,
+        price : 30,
+        category : "Pasta"
+      },
+      {
+        _id : "28",
+        name : "Chicken sphagetti",
+        image : food_28,
+        price : 32,
+        category : "Noodles"
+      },
+      {
+        _id : "29",
+        name : "Pasta with wild greens",
+        image : food_29,
+        price : 30,
+        category : "Pasta"
+      },
+      {
+        _id : "30",
+        name : "Red sphagetti",
+        image : food_30,
+        price : 30,
+        category : "Noodles"
+      },
+      {
+        _id : "31",
+        name : "Prawn noodles",
+        image : food_31,
+        price : 35,
+        category : "Noodles"
+      },
+      {
+        _id : "32",
+        name : "Pho",
+        image : food_32,
+        price : 28,
+        category : "Noodles"
+      }
+]
+
+export const url = 'http://localhost:4000'
